109b590e292cd68783f325dd6a38d481ee29acfb
dapantry <49528969+dapantry@users.noreply.github.com>
2019-04-12 13:27:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function openSlideMenu(){
+  document.getElementById('menu').style.width='250px';
+  document.getElementById('stuff').style.marginLeft='250px';
+}
+function closeSlideMenu(){
+  document.getElementById('menu').style.width='0';
+  document.getElementById('stuff').style.marginLeft='0';
+}
